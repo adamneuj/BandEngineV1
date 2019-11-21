edbe97849a1081d35528e7ae8e27361cf16cd9c2
Adam Neujahr <adamneujahr@gmail.com>
2019-11-21 11:56:25 -0600

    Init

---
+++ BandEngine/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BandEngine.Startup))]
+namespace BandEngine
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
