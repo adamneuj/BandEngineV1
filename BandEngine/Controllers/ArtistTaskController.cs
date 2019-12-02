72ae027ecbc2074fabb09fde6ae84c9e8d9aeab9
Adam Neujahr <adamneujahr@gmail.com>
2019-12-02 11:48:42 -0600

    Built out Index and Create in controller

--- BandEngine/Controllers/ArtistTaskController.cs
+++ BandEngine/Controllers/ArtistTaskController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BandEngine.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,22 @@ namespace BandEngine.Controllers
 {
     public class ArtistTaskController : Controller
     {
+        ApplicationDbContext context;
+        string[] progress;
+
+        public ArtistTaskController()
+        {
+            context = new ApplicationDbContext();
+            progress = new string[3] { "Not Started", "In Progress", "Completed" };
+        }
+
         // GET: ArtistTask
+        [Authorize]
         public ActionResult Index()
         {
-            return View();
+            Artist artist = GetCurrentArtist();
+            List<ArtistTask> currentTasks = context.ArtistTasks.Where(a => a.ArtistId == artist.ArtistId && a.Progress == "Not Started").ToList();
+            return View(currentTasks);
         }
 
         // GET: ArtistTask/Details/5
@@ -23,17 +37,28 @@ namespace BandEngine.Controllers
         // GET: ArtistTask/Create
         public ActionResult Create()
         {
-            return View();
+            ArtistTask artistTask = new ArtistTask();
+            ViewBag.Progress = new SelectList(progress);
+            return View(artistTask);
         }
 
         // POST: ArtistTask/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(ArtistTask artistTask)
         {
             try
             {
-                // TODO: Add insert logic here
-
+                var progress = new SelectList(new[]
+                {
+                    new {value = 1, text = "Not Started"},
+                    new {value = 2, text = "In Progress"},
+                    new {value = 3, text = "Completed"}
+                });
+                ViewBag.Progress = progress;
+                Artist currentArtist = GetCurrentArtist();
+                artistTask.ArtistId = currentArtist.ArtistId;
+                context.ArtistTasks.Add(artistTask);
+                context.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
@@ -85,5 +110,13 @@ namespace BandEngine.Controllers
                 return View();
             }
         }
+
+        private Artist GetCurrentArtist()
+        {
+            var userId = User.Identity.GetUserId();
+            Artist currentArtist = context.Artists.FirstOrDefault(a => a.ApplicationId == userId);
+            return currentArtist;
+        }
+
     }
 }
